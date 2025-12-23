e374a9819aeda9e42417ae8be5fb9a3672173066
Elifsudevural <vuraaleliff@gmail.com>
2025-12-23 18:23:38 +0300

    fark metodu iyileştirildi.

--- src/main/java/tr/edu/medipol/yova/util/calculator/util/Hesaplama.java
+++ src/main/java/tr/edu/medipol/yova/util/calculator/util/Hesaplama.java
@@ -1,21 +1,33 @@
 package tr.edu.medipol.yova.util.calculator.util;
 
 public class Hesaplama {
-	
+
 	public static int topla(int sayi1, int sayi2) {
 		return sayi1 + sayi2;
 	}
-	
+
 	public static int cikar(int sayi1, int sayi2) {
 		return sayi1 - sayi2;
 	}
-	
+
 	public static int carp(int sayi1, int sayi2) {
 		return sayi1 * sayi2;
 	}
-	
+
 	public static int bol(int sayi1, int sayi2) {
 		return sayi1 / sayi2;
+	}
+
+	public static int fark(int sayi1, int sayi2) {
+		if(sayi1 == sayi2) {
+			return 0;
+		}
+		if (sayi1 > sayi2) {
+			return Hesaplama.cikar(sayi1, sayi2);
+		} else {
+			return Hesaplama.cikar(sayi2, sayi1);
+		}
+
 	}
 
 }
